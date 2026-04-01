5b641c031745876e624a53618f6fb54c64443571
Nicolas Liaunardi <liaunardinicolas@gmail.com>
2026-04-01 17:57:44 +0700

    creating user model

---
+++ internal/model/user.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name string
+	Email string `gorm:"unique"`
+	Password string
+	CreatedAt time.Time
+}
